adae25c1bf12408a2f4b941623525d4015309fb6
urlordjames <32751441+urlordjames@users.noreply.github.com>
2019-02-24 22:13:15 -0500

    removed unused var

--- makemodel.py
+++ makemodel.py
@@ -5,7 +5,6 @@ mnist = tf.keras.datasets.mnist #images of hand drawings
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
 
